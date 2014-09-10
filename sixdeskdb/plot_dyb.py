aba1b0e8b3636c180ae9c0877f0d6cc687dcc954
Xavier Valls <xaviervallspla@gmail.com>
2014-09-10 17:23:17 +0200

    added averem, distance, maxslope, smear and survival plots

--- sixdeskdb/plot_dyb.py
+++ sixdeskdb/plot_dyb.py
@@ -26,7 +26,7 @@ rectype=[('seed','int'),('qx','float'),('qy','float'),('betx','float'),('bety','
 names='seed,qx,qy,betx,bety,sigx1,sigy1,deltap,emitx,emity,sigxminnld,sigxavgnld,sigxmaxnld,sigyminnld,sigyavgnld,sigxmaxnld,betx2,bety2,distp,dist,qx_det,qy_det,sturns1,sturns2,turn_max,amp1,amp2,angle,smearx,smeary'
 outtype=[('study','S100'),('seed','int'),('angle','float'),('achaos','float'),('achaos1','float'),('alost1','float'),('alost2','float'),('Amin','float'),('Amax','float')]
 
-def plot_averem(path, nturns):
+def plot_averem(path, nturns, a0, a1):
 
     f22=np.loadtxt('fort.22')
     f23=np.loadtxt('fort.23')
@@ -35,15 +35,91 @@ def plot_averem(path, nturns):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     fig.subplots_adjust(top=0.85)
-    ax.set_title('%s Averaged Amplitude(6d), %s' %(path,nturns))
-
-    ax.set_xlabel('Initial Amplitude [sigma]')
-    ax.set_ylabel('Averaged Amplitude [Sigma]')
     
     ax.plot(f22[:,0], f22[:,1], marker='+', label = "Minimum")
     ax.plot(f23[:,0], f23[:,1], marker='x', label = "Mean")
     ax.plot(f24[:,0], f24[:,1], marker='*', label = "Maximum")
     ax.legend(loc="upper left")
+    ax.set_title('%s Averaged Amplitude(6d), %s' %(path,nturns))
+    ax.set_xlabel('Initial Amplitude [sigma]')
+    ax.set_ylabel('Averaged Amplitude [sigma]')
+    ax.set_xlim(a0,a1)
+    plt.show()
+
+def plot_distance(path, nturns, a0, a1):
+
+    f13=np.loadtxt('fort.13')
+    f26=np.loadtxt('fort.26')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(top=0.85)
+    
+    ax.plot(f13[:,0], f13[:,1], marker='+')
+    ax.plot(f26[:,0], f26[:,1], marker='x', label = "Range from Chaos to Loss")
+    ax.legend(loc="upper left")
+    ax.set_title('%s Averaged Amplitude(6d), %s' %(path,nturns))
+    ax.set_xlabel('Initial Amplitude [sigma]')
+    ax.set_ylabel('Distance in Phase Space of 2 initially close-by Particles')
+    #ax.set_yscale("log", nonposy="clip")
+    ax.set_xlim(a0,a1)
+    plt.show()
+
+def plot_kvar(path, nturns, a0, a1):
+    return
+
+def plot_maxslope(path, nturns, a0, a1):
+    
+    f12=np.loadtxt('fort.12')
+    f26=np.loadtxt('fort.26')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(top=0.85)
+  
+    
+    ax.plot(f12[:,0], f12[:,1], marker='+')
+    ax.plot(f26[:,0], f26[:,1], marker='x', label = "Range from Chaos to Loss")
+    ax.legend(loc="upper left")
+    ax.set_title('%s Averaged Amplitude(6d), %s' %(path,nturns))
+    ax.set_xlabel('Initial Amplitude [sigma]')
+    ax.set_ylabel('Maximum Slope of Distance in Phase Space')
+    ax.set_xlim(a0,a1)
+    plt.show()
+
+def plot_smear(path, nturns, a0, a1):
+
+    f18=np.loadtxt('fort.18')
+    f19=np.loadtxt('fort.19')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(top=0.85)
+    
+    ax.plot(f18[:,0], f18[:,1], marker='+', label="Horizontal" )
+    ax.plot(f19[:,0], f19[:,1], marker='x', label = "Vertical")
+    ax.legend(loc="upper left")
+    ax.set_title('%s Averaged Amplitude(6d), %s' %(path,nturns))
+    ax.set_xlabel('Initial Amplitude [sigma]')
+    ax.set_ylabel('Smear [%]')
+    ax.set_xlim(a0,a1)
+    plt.show()
+
+def plot_survival(path, nturns, a0, a1):
+   
+    f15=np.loadtxt('fort.15')
+    f14=np.loadtxt('fort.14')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(top=0.85)
+
+    ax.plot(f15[:,0], f15[:,1], marker='+')
+    ax.plot(f14[:,0], f14[:,1], marker='x', label = "Chaotic Border")
+    ax.legend(loc="upper left")
+    ax.set_title('%s Averaged Amplitude(6d), %s' %(path,nturns))
+    ax.set_xlabel('Initial Amplitude [sigma]')
+    ax.set_ylabel('Survival Time')
+    #ax.set_yscale("log", nonposy="clip")
+    ax.set_xlim(a0,a1)
+    
     plt.show()
 
 
@@ -166,9 +242,7 @@ def main2(studyName):
                     rad1=np.sqrt(1+rad1*rad1)/sigma
             else:
                 rad1 = 1
-            ich1 = 0
-            ich2 = 0
-            ich3 = 0 
+
             amin=1/epsilon
             amax=zero   
                   
@@ -386,7 +460,13 @@ if __name__ == "__main__":
     if len(args)>1 :
         print "too many options: please provide only <study_name>"
         sys.exit()
-    #main2(sys.argv[1])
+    main2(sys.argv[1])
     path='job_tracking/1/simul/62.31_60.32/6-14/e5/.19'
     nturns=100000
-    plot_averem( '%s/fort10.tgz'%path, nturns)
+    a0 = 6
+    a1 = 14
+    plot_averem( '%s/fort10.tgz'%path, nturns, a0, a1)
+    plot_distance( '%s/fort10.tgz'%path, nturns, a0, a1)
+    plot_maxslope('%s/fort10.tgz'%path, nturns, a0, a1)
+    plot_smear('%s/fort10.tgz'%path, nturns, a0, a1)
+    plot_survival('%s/fort10.tgz'%path, nturns, a0, a1)
