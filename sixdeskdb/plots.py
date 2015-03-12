63886a0ff004bebec27ac322b6994fdc0d45e5af
Xavier Valls <xaviervallspla@gmail.com>
2015-03-12 17:03:22 +0100

    added the option plot to sixdb, which adds the generation of the directory tree and the storage of all postprocessing plots at once

--- sixdeskdb/plots.py
+++ sixdeskdb/plots.py
@@ -2,21 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datafromFort import *
 
-def plot_averem(sd, seed, angle, a0, a1, nturns, path=None):
+postpr_plots = 'averem distance kvar maxslope smear survival'.split()
 
-    f22 = Fort(22, sd)[seed, angle]
-    f23 = Fort(23, sd)[seed, angle]
-    f24 = Fort(24, sd)[seed, angle]
+def plot_averem(db, seed, angle, a0, a1, nturns, path=None):
+
+    f22 = Fort(22, db)[seed, angle]
+    f23 = Fort(23, db)[seed, angle]
+    f24 = Fort(24, db)[seed, angle]
 
     fig = plt.figure()
     ax  = fig.add_subplot(111)
     fig.subplots_adjust(top=0.85)
-    print a0
-    print a1
-    print f22['rad']
-    print f23['rad']
-    print f24['rad']
-    print f23['(rad1*sigxavgnld)']
     ax.plot(f22['rad'], f22['(rad1*sigxminnld)'], marker='+', label = "Minimum")
     ax.plot(f23['rad'], f23['(rad1*sigxavgnld)'], marker='x', label = "Mean")
     ax.plot(f24['rad'], f24['(rad1*sigxmaxnld)'], marker='*', label = "Maximum")
@@ -26,16 +22,15 @@ def plot_averem(sd, seed, angle, a0, a1, nturns, path=None):
     ax.set_xlabel('Initial Amplitude [sigma]')
     ax.set_ylabel('Averaged Amplitude [sigma]')
     ax.set_xlim(a0,a1)
-    # if path:
-    #     plt.savefig("%saverem.png"%path)
-    # else:
-    #     plt.show()
-    plt.show()
+    if path:
+        plt.savefig("%s/averem.%s.%s.png"%(path, nturns, angle))
+    else:
+        plt.show()
 
-def plot_distance(sd, seed, angle, a0, a1, nturns, path=None):
+def plot_distance(db, seed, angle, a0, a1, nturns, path=None):
 
-    f13 = Fort(13, sd)[seed, angle]
-    f26 = Fort(26, sd)[seed, angle]
+    f13 = Fort(13, db)[seed, angle]
+    f26 = Fort(26, db)[seed, angle]
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -51,12 +46,12 @@ def plot_distance(sd, seed, angle, a0, a1, nturns, path=None):
     # ax.semilogy(f13['dist'], np.exp(-f13['dist']/5.0))
     ax.set_xlim(a0,a1)
     if path:
-        plt.savefig("%sdistance.png"%path)
+        plt.savefig("%s/distance.%s.%s.png"%(path, nturns, angle))
     else:
         plt.show()
 
-def plot_kvar(sd, seed, angle, a0, a1, nturns, exponent, path=None):
-    f40 = Fort(40, sd)[seed]
+def plot_kvar(db, seed, angle, a0, a1, nturns, exponent, path=None):
+    f40 = Fort(40, db)[seed]
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -79,14 +74,14 @@ def plot_kvar(sd, seed, angle, a0, a1, nturns, exponent, path=None):
     ax.set_ylabel('Dynamic Aperture in [sigma]')
     ax.set_xlim(0,90)
     if path:
-        plt.savefig("%skvar.png"%path)
+        plt.savefig("%s/kvar.%s.%s.png"%(path, nturns, angle))
     else:
         plt.show()
 
-def plot_maxslope(sd, seed, angle, a0, a1, nturns, path=None):
+def plot_maxslope(db, seed, angle, a0, a1, nturns, path=None):
     
-    f12 = Fort(12, sd)[seed, angle]
-    f26 = Fort(26, sd)[seed, angle]
+    f12 = Fort(12, db)[seed, angle]
+    f26 = Fort(26, db)[seed, angle]
     fig = plt.figure()
     ax = fig.add_subplot(111)
     fig.subplots_adjust(top=0.85)
@@ -100,14 +95,14 @@ def plot_maxslope(sd, seed, angle, a0, a1, nturns, path=None):
     ax.set_ylabel('Maximum Slope of Distance in Phase Space')
     ax.set_xlim(a0,a1)
     if path:
-        plt.savefig("%smaxslope.png"%path)
+        plt.savefig("%s/maxslope.%s.%s.png"%(path, nturns, angle))
     else:
         plt.show()
 
-def plot_smear(sd, seed, angle, a0, a1, nturns, path=None):
+def plot_smear(db, seed, angle, a0, a1, nturns, path=None):
 
-    f18 = Fort(18, sd)[seed, angle]
-    f19 = Fort(19, sd)[seed, angle]
+    f18 = Fort(18, db)[seed, angle]
+    f19 = Fort(19, db)[seed, angle]
     fig = plt.figure()
     ax = fig.add_subplot(111)
     fig.subplots_adjust(top=0.85)
@@ -120,14 +115,14 @@ def plot_smear(sd, seed, angle, a0, a1, nturns, path=None):
     ax.set_ylabel('Smear [%]')
     ax.set_xlim(a0,a1)
     if path:
-        plt.savefig("%ssmear.png"%path)
+        plt.savefig("%s/smear.%s.%s.png"%(path, nturns, angle))
     else:
         plt.show()
 
-def plot_survival(sd, seed, angle, a0, a1, nturns, path=None):
+def plot_survival(db, seed, angle, a0, a1, nturns, path=None):
    
-    f15 = Fort(15, sd)[seed, angle]
-    f14 = Fort(14, sd)[seed, angle]
+    f15 = Fort(15, db)[seed, angle]
+    f14 = Fort(14, db)[seed, angle]
     fig = plt.figure()
     ax  = fig.add_subplot(111)
     fig.subplots_adjust(top=0.85)
@@ -143,14 +138,53 @@ def plot_survival(sd, seed, angle, a0, a1, nturns, path=None):
     ax.set_xlim(a0, a1)
     
     if path:
-        plt.savefig("%ssurvival.png"%path)
+        plt.savefig("%s/survival.%s.%s.png"%(path, nturns, angle))
     else:
         plt.show()
 
+# def clean_dir_plot(db,files):
+#   '''create directory structure and if force=true delete old files of plots'''
+#   for seed in db.get_seeds():
+#     for tune in db.get_db_tunes():
+#       pp=db.mk_analysis_dir(seed,tune)# create directory
+#       if(len(files)>0):#delete old plots and files
+#         for filename in files:
+#           ppf=os.path.join(pp,filename)
+#           if(os.path.exists(ppf)): os.remove(ppf)
+#   if(len(files)>0):
+#     print('remove old {0} ... files in '+db.LHCDescrip).format(files)
+
+def plotPostProcessingPlots(db):
+  '''plot survival plots and da vs turns for list of data ldat and associated error ldaterr'''
+  turnsl=db.env_var['turnsl']
+  turnse=db.env_var['turnse']
+  a0 = db.env_var['ns1l']
+  a1 = db.env_var['ns2l']
+  print('Post processing plots -- generating the post processing plots plots')
+  
+  #files = postpr_plot_files = [p+'%s.%s.png'%(turnse,angle) for p in postpr_plots]
+  # clean_dir_da_vst(db,files)# create directory structure and delete old files if force=true
+  if(not db.check_seeds()):
+    print('!!! Seeds are missing in database !!!')
+  for seed in db.get_seeds():
+    seed=int(seed)
+    for tune in db.get_db_tunes():
+      dirname=db.mk_analysis_dir(seed,tune)#directory struct already created in clean_dir_da_vst, only get dir name (string) here
+      for angle in xrange(len(db.get_db_angles())):
+          plt.close('all')
+          plt.figure(figsize=(6,6))
+          plot_averem(db, seed, angle, a0, a1, turnsl, path=dirname)
+          plot_distance(db, seed, angle, a0, a1, turnsl, path=dirname)
+          plot_kvar(db, seed, angle, a0, a1, turnsl, turnse, path=dirname)
+          plot_maxslope(db, seed, angle, a0, a1, turnsl, path=dirname)
+          plot_smear(db, seed, angle, a0, a1, turnsl, path=dirname)
+          plot_survival(db, seed, angle, a0, a1, turnsl, path=dirname)
+
+
 #short
-# def plot_tunedp(sd, seed, angle, a0, a1, nturns, path=None):
-#     f16=Fort(16, sd)
-#     f17=Fort(17, sd)
+# def plot_tunedp(db, seed, angle, a0, a1, nturns, path=None):
+#     f16=Fort(16, db)
+#     f17=Fort(17, db)
 #     fig = plt.figure()
 #     ax = fig.add_subplot(111)
 #     fig.subplots_adjust(top=0.85)
