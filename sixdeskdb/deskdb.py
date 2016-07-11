a4536e99485cfb5cd770f4e896fd457b6bbf60f6
mfittere <miriam.fitterer@cern.ch>
2016-07-11 10:08:29 -0500

    correct plot label for plot_fma_scatter

--- sixdeskdb/deskdb.py
+++ sixdeskdb/deskdb.py
@@ -1984,7 +1984,7 @@ class SixDeskDB(object):
 # q1={}
 # q1.setdefault((a1,a2),[]).append(...)
     if len(files) < 2:
-      raise Exception("ERROR in plot_fma_scatter: you need to define at least 2 (inputfile,method) pairs to calcute the difference in tune!!!")
+      raise Exception("ERROR in get_fma_intersept: you need to define at least 2 (inputfile,method) pairs to calcute the difference in tune!!!")
     (tunex,tuney)=tune
     nfma = len(files) # number of (inputfile,method)
     if(self.check_view('fma')):
@@ -2211,8 +2211,8 @@ class SixDeskDB(object):
         print 'WARNING: Table da_post does not exist! To create it and plot the DA, please run db.mk_da()!'
       else:
         self.plot_da_angle_seed(seed,marker=None,linestyle='-',color='k')
-      pl.xlabel(r'$\sigma_x=\frac{\epsilon_{1,%s}}{\epsilon_0}, \ \epsilon_{0,N}=\epsilon_0/\gamma = %2.2f \ \mu \rm m$'%(var1.split('_')[1],self.env_var['emit']))
-      pl.ylabel(r'$\sigma_y=\frac{\epsilon_{2,%s}}{\epsilon_0}, \ \epsilon_{0,N}=\epsilon_0/\gamma = %2.2f \ \mu \rm m$'%(var2.split('_')[1],self.env_var['emit']))
+      pl.xlabel(r'$\sigma_x=\sqrt{\frac{\epsilon_{1,%s}}{\epsilon_0}}, \ \epsilon_{0,N}=\epsilon_0/\gamma = %2.2f \ \mu \rm m$'%(var1.split('_')[1],self.env_var['emit']))
+      pl.ylabel(r'$\sigma_y=\sqrt{\frac{\epsilon_{2,%s}}{\epsilon_0}}, \ \epsilon_{0,N}=\epsilon_0/\gamma = %2.2f \ \mu \rm m$'%(var2.split('_')[1],self.env_var['emit']))
 # tune vs dq
     if('q' in var1 and 'q' in var2):
       pl.scatter(data['fma0_%s'%var1],data['fma0_%s'%var2],c=dq,marker='.',linewidth=0,vmin=vmin,vmax=vmax,s=5)
